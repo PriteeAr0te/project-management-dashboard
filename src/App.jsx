b62da48d8c8064f54e08d8794492003b6ba73dd5
PriteeAr0te <arotepritee111@gmail.com>
2025-09-15 21:02:22 +0530

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-center h-[100vh] w-[100vw] text-blue-600 font-semibold">Product Management Dashboard</h1>
+    </>
+  )
+}
+
+export default App
